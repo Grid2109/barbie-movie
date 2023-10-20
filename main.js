7812c5f464fa6e523ff5900836e31ee472bfe716
Ingrid Lira <gridferreiradoa2109@gmail.com>
2023-10-20 17:53:32 -0300

    acrescentei uma function no javascript e media queries ao css

--- main.js
+++ main.js
@@ -18,14 +18,31 @@ function playAudio() {
     let speakerSlash = document.getElementsByClassName('ph-speaker-slash')[0];
     let speakerHigh = document.getElementsByClassName('ph-speaker-high')[0];
 
-    if (!audio.paused && !audio.ended) {
-        audio.pause();
-        speakerSlash.style.display = "block";
-        speakerHigh.style.display = "none";
-      } else {
-        audio.play();
-        speakerSlash.style.display = "none";
-        speakerHigh.style.display = "block";
-      }
+    if (audio.paused) { 
+      audio.play(); // Inicia a reprodução do áudio
+      speakerSlash.style.display = "none"; // Oculta o ícone do alto-falante com barra(audio mudo)
+      speakerHigh.style.display = "block"; // Exibe o ícone do alto-falante (musica reproduzindo)
+    } else {
+      audio.pause(); // Pausa a reprodução do áudio
+      speakerSlash.style.display = "block"; // Exibe o ícone do alto-falante com barra(audio mudo)
+      speakerHigh.style.display = "none"; // Oculta o ícone do alto-falante(musica reproduzindo)
+    }
+     
+}
+
+function muteAudio() {
+    let audio = document.getElementById('music');
+    let speakerSlash = document.getElementsByClassName('ph-speaker-slash')[0];
+    let speakerHigh = document.getElementsByClassName('ph-speaker-high')[0];
+
+    if (audio.paused) {
+      audio.play();
+      speakerSlash.style.display = "none";
+      speakerHigh.style.display = "block";
+    } else {
+      audio.pause();
+      speakerSlash.style.display = "block";
+      speakerHigh.style.display = "none";
+    }
 }
 
